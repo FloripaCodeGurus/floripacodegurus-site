6dab7be0f853d01543a3281d4952f87d01fac3ba
agilproducts <agilfiles2023@gmail.com>
2025-07-10 13:55:29 -0300

    update method creation

--- tutoriais/views.py
+++ tutoriais/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Tutoriais
 
 
@@ -7,8 +8,8 @@ def tutoriais(request):
     return render(request, 'tutoriais/tutoriais.html', {'tutoriais': tutoriais})
 
 
-def tutoriais2(request):
-    return render(request, 'tutoriais/tutoriais2.html', {})
+# def tutoriais2(request):
+#     return render(request, 'tutoriais/tutoriais2.html', {})
 
 
 # def currencylayer(request):
@@ -26,8 +27,36 @@ def tutoriais2(request):
 # def bio001(request):
 #     return render(request, 'tutoriais/bio_001.html', {})
 
+
 def tutorial_create(request):
+    if request.method == 'POST':
+        tutorial = Tutoriais(
+            titulo=request.POST.get('titulo'),
+            descricao=request.POST.get('descricao'),
+            introducao=request.POST.get('introducao'),
+            conceitos=request.POST.get('conceitos'),
+            exemplos=request.POST.get('exemplos'),
+            conclusao=request.POST.get('conclusao'),
+            autor=request.POST.get('autor'),
+            categoria=request.POST.get('categoria'),
+            nivel=request.POST.get('nivel')
+        )
+        if request.FILES.get('imagem'):
+            tutorial.imagem = request.FILES['imagem']
+        tutorial.save()
+        messages.success(request, 'Tutorial criado com sucesso!')
+        return redirect('tutorial_detail', slug=tutorial.slug)
     return render(request, 'tutoriais/tutorial_create.html', {})
+
+
+def tutorial_list(request):
+    tutoriais = Tutoriais.objects.all().order_by('-data_criacao')
+    return render(request, 'tutoriais/tutorial_list.html', {'tutoriais': tutoriais})
+
+
+def tutorial_detail(request, slug):
+    tutorial = get_object_or_404(Tutoriais, slug=slug)
+    return render(request, 'tutoriais/tutorial_detail.html', {'tutorial': tutorial})
 
 
 def tutoriais_lista(request):
@@ -48,13 +77,3 @@ def tutoriais_lista(request):
 def tutoriais_detalhe(request, slug):
     tutorial = get_object_or_404(Tutoriais, slug=slug)
     return render(request, 'tutoriais/tutoriais_detalhe.html', {'tutorial': tutorial})
-                                                              
-
-def tutorial_list(request):
-    tutoriais = Tutoriais.objects.all().order_by('-data_criacao')
-    return render(request, 'tutoriais/tutorial_list.html', {'tutoriais': tutoriais})                                   
-
-
-def tutorial_detail(request, slug):
-    tutorial = get_object_or_404(Tutoriais, slug=slug)
-    return render(request, 'tutoriais/tutorial_detail.html', {'tutorial': tutorial})
