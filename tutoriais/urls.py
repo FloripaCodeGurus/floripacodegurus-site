2f1963e27a1dc192d98daac47f120615e8c884bf
agilproducts <agilfiles2023@gmail.com>
2025-07-10 17:45:27 -0300

    tutorials create, list, detail

--- tutoriais/urls.py
+++ tutoriais/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.tutoriais, name='tutoriais'),
-    path('page2/', views.tutoriais2, name='tutoriais2'),
-    path('bio001/', views.bio001, name='bio001'),
-    path('currencylayer/', views.currencylayer, name='currencylayer'),
-    path('histogramas/', views.histogramas, name='histogramas'),
-    path('regex/', views.regex, name='regex'),
+    # path('page2/', views.tutoriais2, name='tutoriais2'),
+    # path('bio001/', views.bio001, name='bio001'),
+    # path('currencylayer/', views.currencylayer, name='currencylayer'),
+    # path('histogramas/', views.histogramas, name='histogramas'),
+    # path('regex/', views.regex, name='regex'),
     path('lista/', views.tutoriais_lista, name='tutoriais_lista'),
     path('list/', views.tutorial_list, name='tutorial_list'),
     path('create/', views.tutorial_create, name='tutorial_create'),
