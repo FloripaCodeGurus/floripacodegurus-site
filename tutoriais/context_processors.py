05d62a10c3dd3ad0ca48df3560269d69fa12707c
agilproducts <agilfiles2023@gmail.com>
2025-07-14 20:54:30 -0300

    Added Posts Populares filter

--- tutoriais/context_processors.py
+++ tutoriais/context_processors.py
@@ -1,9 +1,19 @@
 from .models import Tutoriais
 
 def latest_tutorials(request):
-    """Context processor to provide latest 3 tutorials to all templates"""
+    """Context processor to provide latest 3 tutorials and categories to all templates"""
     try:
         latest_tutorials = Tutoriais.objects.all().order_by('-data_criacao')[:3]
-        return {'latest_tutorials': latest_tutorials}
+        categorias = Tutoriais.objects.values_list('categoria', flat=True).distinct()
+        categorias = sorted(set([c.strip().title() for c in categorias if c]))
+        return {
+            'latest_tutorials': latest_tutorials,
+            'categorias': categorias,
+            'categoria_ativa': request.GET.get('categoria')
+        }
     except:
-        return {'latest_tutorials': []}
+        return {
+            'latest_tutorials': [],
+            'categorias': [],
+            'categoria_ativa': None
+        }
