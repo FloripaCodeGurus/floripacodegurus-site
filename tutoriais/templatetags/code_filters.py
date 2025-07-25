3ed1e970c0ce7b0a296644f192b1759efb68b605
agilproducts <agilfiles2023@gmail.com>
2025-07-25 19:58:09 -0300

    Trying to use preserve_code tag

--- tutoriais/templatetags/code_filters.py
+++ tutoriais/templatetags/code_filters.py
@@ -4,15 +4,23 @@ import re
 
 register = template.Library()
 
-@register.filter(name='format_code')
-def format_code(value):
+@register.filter(name='preserve_code')
+def preserve_code(value):
     """
-    Filter to properly format code blocks by removing HTML tags
-    while preserving whitespace and indentation
+    Filter to preserve code formatting exactly as entered
     """
     if value:
-        # Remove HTML tags but preserve line breaks and spaces
-        clean_code = re.sub(r'<[^>]*>', '', value)
-        # Ensure the output is marked safe to prevent further escaping
+        # Just remove HTML tags and decode entities, preserve everything else
+        clean_code = re.sub(r'<[^>]*>', '', str(value))
+        clean_code = clean_code.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
+        clean_code = clean_code.replace('&nbsp;', ' ')
+        # Remove excessive whitespace between words but preserve line structure
+        lines = clean_code.split('\n')
+        cleaned_lines = []
+        for line in lines:
+            # Only clean up excessive spaces within lines, preserve indentation
+            cleaned_line = re.sub(r' +', ' ', line.rstrip())
+            cleaned_lines.append(cleaned_line)
+        clean_code = '\n'.join(cleaned_lines).strip()
         return mark_safe(clean_code)
     return ''
